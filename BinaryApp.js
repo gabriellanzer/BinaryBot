34b6e4b5a1c45ff5b07bf3b7ee0305a0974524f4
Gabriel Lanzer Kannenberg <gabriellanzerlive@hotmail.com>
2019-08-22 20:21:00 -0300

    Added custom ConsoleLogs with different colors, fixed SingleUses callings, fixed a bunch of other stuff, reorganized BinaryApp class, changed the way Use directive works, created stalls until socket is actually connected, mapped live contracts and profit from transacions, and more...

--- BinaryApp.js
+++ BinaryApp.js
@@ -1,78 +1,158 @@
-WebSocket = require("ws");
+require("./ConsoleLogs.js")()
+
+WebSocket = require("ws")
 
 class BinaryApp {
   constructor(url) {
-    this.ws = new WebSocket(url);
-    this.uses = {};
-    setInterval(this.Ping.bind(this), 4000);
-    this.ws.onmessage = this.ChainCall.bind(this);
+    this.ws = new WebSocket(url)
+    this.uses = {}
+    this.singleUses = {}
+    this.ws.onmessage = this.ChainCall.bind(this)
+    this.timeout = 10000
+    this.timeoutCounter = this.timeout
+    this.authorized = false
+    setInterval(this.Ping.bind(this), 4000)
   }
 
-  Use(msg_type, callback) {
-    if (!this.uses[msg_type]) this.uses[msg_type] = [];
-    this.uses[msg_type][this.uses[msg_type].length] = callback;
-  }
-
-  ChainCall(msg) {
-    const data = JSON.parse(msg.data);
-    if (!this.uses[data.msg_type]) return;
-    if (this.uses[data.msg_type][0]) {
-      this.uses[data.msg_type][0](
-        data,
-        this.uses[data.msg_type][1] ? this.uses[data.msg_type][1] : () => {}
-      );
-    }
-  }
-
-  Authorize(API_KEY) {
-    this.ws.send(
-      JSON.stringify({
-        authorize: API_KEY
-      })
-    );
+  Authorize(API_KEY, onAuthorized) {
+    this.Send({
+      authorize: API_KEY
+    })
+    this.SingleUse("authorize", msg => {
+      if (msg.error) {
+        console.logError(msg.error.message)
+      } else {
+        this.authorized = true
+      }
+    })
+    if (onAuthorized) this.SingleUse("authorize", onAuthorized)
   }
 
   PriceProposal() {
-    this.ws.send(
-      JSON.stringify({
-        proposal: 1,
-        amount: 1,
-        basis: "payout",
-        contract_type: "CALL",
-        currency: "USD",
-        duration: 60,
-        duration_unit: "s",
-        barrier: "+0.1",
-        symbol: "R_100"
-      })
-    );
+    this.Send({
+      proposal: 1,
+      amount: 1,
+      basis: "payout",
+      contract_type: "CALL",
+      currency: "USD",
+      duration: 60,
+      duration_unit: "s",
+      barrier: "+0.1",
+      symbol: "R_100"
+    })
   }
 
   BuyContract(price, parameters, buy = 1) {
-    thiw.ws.send(
-      JSON.stringify({
-        buy: buy,
-        price: price,
-        parameters: parameters
-      })
-    );
+    this.Send({
+      buy: buy,
+      price: price,
+      parameters: parameters
+    })
   }
 
   SendExpiredContract() {
-    thiw.ws.send(
-      JSON.stringify({
-        sell_expired: 1
-      })
-    );
+    this.Send({
+      sell_expired: 1
+    })
   }
 
   Ping() {
-    this.ws.send(
-      JSON.stringify({
-        ping: 1
+    this.Send({
+      ping: 1
+    })
+  }
+
+  CheckConnection(delay) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        if (this.ws.readyState === WebSocket.OPEN) {
+          resolve(true)
+        } else {
+          resolve(false)
+        }
+      }, delay)
+    })
+  }
+
+  async WaitConnection() {
+    let connected = this.ws.readyState === WebSocket.OPEN
+    while (!connected) {
+      this.connected = await this.CheckConnection(200)
+      if (this.connected) {
+        this.timeoutCounter = this.timeout
+        break
+      } else {
+        this.timeoutCounter -= 200
+        if (this.timeoutCounter <= 0) {
+          this.timeoutCounter = this.timeout
+          throw "Timed-out while waiting for connection..."
+        }
+      }
+    }
+  }
+
+  async Send(data) {
+    try {
+      await this.WaitConnection()
+    } catch (err) {
+      console.logError(err)
+    }
+    try {
+      this.ws.send(JSON.stringify(data))
+    } catch (err) {
+      console.logError("Error sending message: " + err)
+    }
+  }
+
+  Use(msg_type, callback) {
+    if (!callback) throw "Use callback undefined!"
+    if (typeof callback !== "function") throw "Use callback is not a function!"
+    if (!this.uses[msg_type]) this.uses[msg_type] = []
+    this.uses[msg_type][this.uses[msg_type].length] = callback
+  }
+
+  SingleUse(msg_type, callback) {
+    if (!callback) throw "SingleUse callback undefined!"
+    if (typeof callback !== "function")
+      throw "SingleUse callback is not a function!"
+    if (!this.singleUses[msg_type]) this.singleUses[msg_type] = []
+    this.singleUses[msg_type][this.singleUses[msg_type].length] = callback
+  }
+
+  ChainCallIt(msg, type, it) {
+    if (this.uses[type][it]) {
+      this.uses[type][it](
+        msg,
+        (() => {
+          this.ChainCallIt(msg, type, it + 1)
+        }).bind(this)
+      )
+    }
+  }
+
+  ChainSingleCall(msg, type) {
+    if (this.singleUses[type]) {
+      this.singleUses[type].forEach(call => {
+        call(msg)
       })
-    );
+      this.singleUses[type].length = 0
+    }
+  }
+
+  ChainCall(msg) {
+    const data = JSON.parse(msg.data)
+    let type = data.msg_type
+    if (!this.singleUses[data.msg_type]) {
+      type = ""
+    }
+    this.ChainSingleCall(data, type)
+
+    type = data.msg_type
+    if (!this.uses[data.msg_type]) {
+      type = ""
+    }
+    this.ChainCallIt(data, type, 0)
   }
 }
 
-module.exports = BinaryApp;
+module.exports = BinaryApp
--- app.js
+++ app.js
@@ -1,36 +1,106 @@
-BinaryApp = require("./BinaryApp.js");
+require("./ConsoleLogs.js")()
 
-let App = new BinaryApp("wss://ws.binaryws.com/websockets/v3?app_id=1089");
+BinaryApp = require("./BinaryApp.js")
 
-App.Use('authorize', (msg, next) => { console.log(msg) })
-App.Use('ping', (msg, next) => { console.log('Pong!') })
+let App = new BinaryApp("wss://ws.binaryws.com/websockets/v3?app_id=1089")
 
+App.Use("authorize", (msg, next) => {
+  if (msg.error) {
+    console.logError(msg.error.message)
+  } else {
+    console.logNotify("Authenticated!")
+  }
+  next()
+})
 
-setTimeout(() => {App.Authorize('wgMRHgA5l2MCXbv')}, 1000)
+App.Use("buy", (msg, next) => {
+  if (msg.error) {
+    console.logError("Buy failed with message " + msg.error.message)
+  } else {
+    liveContracts.set(msg.buy.contract_id, msg.buy)
+  }
+  next()
+})
 
-// ws.onopen = function(evt) {
-//   ws.send(App.Authorize("wgMRHgA5l2MCXbv"));
-// };
+App.Use("tick", (msg, next) => {
+  if (msg.error) {
+    console.logError("Error receiving tick update " + msg.error.message)
+  } else {
+    //console.logInfo("Tick Value Received: " + msg.tick.quote)
+  }
+  next()
+})
 
-// ws.onmessage = function(msg) {
-//   var data = JSON.parse(msg.data);
-//   console.log("Response: %o", data);
-// };
+let i = 0
+App.Use("tick", (msg, next) => {
+  i = (i + 1) % 2 //Actions every 2 ticks
+  if (i == 0) {
+    App.BuyContract(10, {
+      symbol: "R_100", //Volatility 100
+      currency: "USD", //Same as account
+      contract_type: "CALL", //CALL - Going Up
+      duration_unit: "t", //Tick
+      duration: 1, //1 Tick
+      basis: "stake",
+      amount: 1
+    })
+  }
+  next()
+})
 
-// let stake = 1;
+const liveContracts = new Map()
+App.Use("transaction", (msg, next) => {
+  if (msg.error) {
+    console.logError("Transaction error: " + msg.error.message)
+  } else {
+    const trs = msg.transaction
+    if (trs.action == "sell") {
+      const value = trs.amount - liveContracts.get(trs.contract_id).buy_price
+      if (value > 0) {
+        console.logWin(`Sold contract with profit: ${value} ${trs.currency}`)
+      } else {
+        console.logLoss(`Sold contract with loss: ${value} ${trs.currency}`)
+      }
+      liveContracts.delete(trs.contract_id)
+    } else if (trs.action == "buy") {
+      console.logWarning(
+        `Bought contract for ${Math.abs(trs.amount)} ${trs.currency}`
+      )
+    }
+  }
+  next()
+})
 
-// //Set tick update loop
-// setInterval(() => {
-//   ws.send(
-//     BuyContract(stake, {
-//       symbol: "R_100", //Volatility 100
-//       currency: "USD", //Same as account
-//       contract_type: "CALL", //CALL - Going Up
-//       duration_unit: "t", //Tick
-//       duration: 1, //1 Tick
-//       basis: "stake",
-//       amount: stake
-//     })
-//   );
-//   ws.send(SendExpiredContract());
-// }, 2000);
+App.Use("", (msg, next) => {
+  if (msg.error) {
+    console.logError(msg.error.message)
+  }
+  next()
+})
+
+App.Authorize("wgMRHgA5l2MCXbv", msg => {
+  App.Send({
+    ticks: "R_100"
+  })
+  App.Send({
+    transaction: 1,
+    subscribe: 1
+  })
+})
+
+let stdin = process.openStdin()
+
+stdin.addListener("data", function(input) {
+  const data = input.toString().trim()
+  if (data == "balance") {
+    App.Send({
+      "balance": 1,
+      "subscribe": 0
+    })
+    App.SingleUse("balance", (msg) => {
+      console.logInfo(
+        `Current Balance = ${msg.balance.balance} ${msg.balance.currency}`
+      )
+    })
+  }
+})
